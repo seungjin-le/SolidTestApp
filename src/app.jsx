4ded33e46483cd7883188fc5b42fcd86f31fa599
Lee Seung Jin <dltmdwls154@gmail.com>
2024-08-17 22:47:39 +0900

    Solid.js React랑 별 차이 없네

--- src/app.jsx
+++ src/app.jsx
@@ -1,20 +1,19 @@
-import { Router } from "@solidjs/router";
-import { FileRoutes } from "@solidjs/start/router";
-import { Suspense } from "solid-js";
+import { Router } from '@solidjs/router'
+import { FileRoutes } from '@solidjs/start/router'
+import { Suspense } from 'solid-js'
 
-import "./app.css";
+import './app.css'
 
 export default function App() {
   return (
     <Router
-      root={props => (
-
-
-          <Suspense>{props.children}</Suspense>
-   
+      root={(props) => (
+        <Suspense>
+          <div>{props.children}</div>
+        </Suspense>
       )}
     >
       <FileRoutes />
     </Router>
-  );
+  )
 }
--- src/routes/test/[id].jsx
+++ src/routes/test/[id].jsx
@@ -1,6 +1,6 @@
-import { useParams } from "@solidjs/router";
+import { useParams } from '@solidjs/router'
 
-export default function() {
-  const { id } = useParams();
-  return <div>sdf{id}</div>;
+export default function () {
+  const { id } = useParams()
+  return <div>sdf{id}</div>
 }
